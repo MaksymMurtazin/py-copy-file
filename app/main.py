c3b87b409e509c1946b034dd5255d15a2e858476
Maksym Murtazin <max.murtazin2006@gmail.com>
2026-07-16 18:09:51 +0300

    Combined the check for the correctness of the command into a single line.

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from os.path import exists
 def copy_file(command: str) -> None:
     parts_of_command = command.split()
 
-    if len(parts_of_command) == 3:
-        if ((parts_of_command[1] != parts_of_command[2])
-                and parts_of_command[0] == "cp"):
-            if exists(parts_of_command[1]):
-                with (open(parts_of_command[1], "r") as existing_file,
-                      open(parts_of_command[2], "w") as file_copy):
-                    file_copy.write(existing_file.read())
+    if (len(parts_of_command) == 3
+            and parts_of_command[1] != parts_of_command[2]
+            and parts_of_command[0] == "cp"):
+        if exists(parts_of_command[1]):
+            with (open(parts_of_command[1], "r") as existing_file,
+                  open(parts_of_command[2], "w") as file_copy):
+                file_copy.write(existing_file.read())
